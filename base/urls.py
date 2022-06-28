54a28b4e3f142d47d9115f3217513214037c7fc1
Aitchessbee <bediharsiddak@gmail.com>
2022-06-28 22:59:46 +0530

    Added edit functionality

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('addContact/', views.addContact, name="addContact"),
-    path('deleteContact/<str:pk>/', views.deleteContact, name="deleteContact")
+    path('deleteContact/<str:pk>/', views.deleteContact, name="deleteContact"),
+    path('editContact/<str:pk>/', views.editContact, name="editContact")
 ]
--- base/views.py
+++ base/views.py
@@ -29,3 +29,17 @@ def deleteContact(request, pk):
 
     context = {"contact": contact}
     return render(request, 'base/delete.html', context)
+
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+
+    if request.method == "POST":
+
+        contact.name = request.POST.get("name")
+        contact.number = request.POST.get("number")
+
+        contact.save()
+        
+
+    context = {"contact": contact}
+    return render(request, 'base/edit.html', context)
