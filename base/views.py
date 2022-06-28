f8961afb9fa973163cd2a6fb5dc527e6423c9ba5
Aitchessbee <bediharsiddak@gmail.com>
2022-06-28 21:30:13 +0530

    connected database to app

--- base/views.py
+++ base/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Contact
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    contacts = Contact.objects.all()
+    context = {"contacts": contacts}
+
+    return render(request, 'base/home.html', context)
 
 def addContact(request):
-    
-
+    if request.method == "POST":
+        Contact.objects.create(
+            name = request.POST.get("name"),
+            number = request.POST.get("number")
+        )
 
     return render(request, 'base/contact.html')
